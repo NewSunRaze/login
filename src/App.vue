c807044580bc7b848c917b8c507e04ad3e337779
andrey <aidyatullin@biplanum.ru>
2024-10-04 00:08:36 +0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
   <router-view />
 </template>
+
+<script setup>
+import { useAuthStore } from './stores/auth'
+import { onMounted } from 'vue'
+
+const authStore = useAuthStore()
+onMounted(() => {
+  console.log('log from App.vue')
+})
+</script>
